e3f9e81d07bcab38d9580b01021f8a9405607e9c
Daniel Hasselwander <daniel.hasselwander@nttdata.com>
2018-03-19 20:35:27 +0100

    int

---
+++ src/main/java/dhbw/leftlovers/angebotsservice/AngebotsserviceApplication.java
@@ -0,0 +1,12 @@
+package dhbw.leftlovers.angebotsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngebotsserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngebotsserviceApplication.class, args);
+	}
+}
---
+++ src/test/java/dhbw/leftlovers/angebotsservice/AngebotsserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package dhbw.leftlovers.angebotsservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AngebotsserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
